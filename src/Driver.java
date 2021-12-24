f4fb70018cb090ce7ae8c9cce7f709af392a2fde
deepa <deepa@VnA-WinLaptop>
2021-12-24 10:02:23 -0800

    with scanner

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TestBranch TB=new TestBranch();
-		TB.operations("sum");
+		Scanner sc= new Scanner(System.in);
+		String input=sc.nextLine();
+		TB.operations(input);
 	}
 
 }
